af4ec169d8de9d7f7517df7302f7c4dfccb0fbf3
unknown <ustun.melih.94@gmail.com>
2021-11-11 15:59:06 +0300

    son

--- 04-javascript-kategorisi/js/011-prompt-ile-kullanicidan-bilgi-almak.js
+++ 04-javascript-kategorisi/js/011-prompt-ile-kullanicidan-bilgi-almak.js
@@ -6,3 +6,4 @@ let fullName = prompt("Lutfen Adinizi Giriniz: ")
 let greeting = document.querySelector("#greeting")
 
 greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`
+
