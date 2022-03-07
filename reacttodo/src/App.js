ef77420eb5afc958fb588fd9e86e8d5eb6a57aa9
ustunmelih <ustun.melih.94@gmail.com>
2022-03-07 16:33:41 +0300

    todoEnd

--- reacttodo/src/App.js
+++ reacttodo/src/App.js
@@ -1,42 +1,39 @@
-import { FormControl, Button, Form } from 'react-bootstrap'
-import { useEffect, useState } from 'react'
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import TodoItem from "./components/TodoItem";
+import TodoInput from "./components/TodoInput";
+
 function App() {
-  const [todoList, setTodoList] = useState([])
-  const [todo, setTodo] = useState('')
-  const addTodo = () => {
-    setTodoList((prevTodoList) => [...prevTodoList, todo])
-    setTodo('')
-  }
+  const [todoList, setTodoList] = useState([]);
+  useEffect(() => {
+    console.log(todoList);
+    setTodoList((todoList) =>
+      todoList.sort((a, b) =>
+        a.isCompleted === b.isCompleted ? 0 : !a.isCompleted ? -1 : 1
+      )
+    );
+  }, [todoList]);
 
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center mt-5">
-      <h1 className="mt-5">Todo App</h1>
-
-      <div className="d-flex w-50" mt-3>
-        <FormControl
-          className="w-75"
-          placeholder="Todo Input"
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
-        />
-        <Button className="ms-5" onClick={addTodo}>
-          Add Todo
-        </Button>
-      </div>
-      <div className="mt-5 w-75">
-        {todoList.map((todoItem) => (
-          <div key={todoItem} className="d-flex justify-content-between">
-            <div className="d-flex">
-              <Form.Check type="checkbox" className="me-2" />
-
-              <label>{todoItem}</label>
-            </div>
-            <div>butonlar</div>
-          </div>
-        ))}
+    <div
+      className="w-100 d-flex justify-content-center"
+      style={{ paddingTop: "15vh" }}
+    >
+      <div className="w-75 d-flex align-items-center flex-column">
+        <h1>Todo List</h1>
+        <TodoInput setTodoList={setTodoList} />
+        <div className="d-flex mt-3 flex-column w-75">
+          {todoList
+            .sort((a, b) =>
+              a.isCompleted === b.isCompleted ? 0 : !a.isCompleted ? -1 : 1
+            )
+            ?.map((item) => (
+              <TodoItem key={item.id} item={item} setTodoList={setTodoList} />
+            ))}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
