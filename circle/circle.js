e92b7d6f93774bc822b3adfcdb0edeccf87046b3
ustunmelih <ustun.melih.94@gmail.com>
2022-03-20 19:56:48 +0300

    circle

---
+++ circle/circle.js
@@ -0,0 +1,7 @@
+exports.circleArea = (r) => {
+  return console.log('circleArea: ', Math.PI * r * r)
+}
+
+exports.circleCircumference = (r) => {
+  return console.log('circleCircumference: ', Math.PI * r * 2)
+}
